0ec521ef9f1be5e9ef53508f2f5655d5cbcc0517
jhonbg93 <jhon.betancourth03@gmail.com>
2022-03-08 06:46:29 -0500

    carga de imagen login

--- src/components/SliderLogin.jsx
+++ src/components/SliderLogin.jsx
@@ -3,9 +3,12 @@ function SliderLogin(){
     return(
         <div className="text-center " >
             <div className="mt-1 mb-16 mx-20 bg-[#E487FB] rounded-full h-96 w-96 "  >
-                
+                <div>
+                    <img className=" my-10" src="image/loginMenu/img2.png" alt="" />
+                </div>
             </div>
             <div className="mb-0">
+                
             <h2 class="text-5xl font-roboto text-white text-clip "><b>anime<span className="text-[#CA38ED]">yabu.</span></b> </h2>
                 <p className="text-white my-1 text-xs ">Ver anime en línea en HD, subtitulado o doblado,<br/>en tu celular o computadora.<br/>
                 ¡Animeyabu, tu portal de anime en línea! </p>
